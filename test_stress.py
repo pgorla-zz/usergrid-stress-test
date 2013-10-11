f6dca6a3507ba2b8402e233f45fec44c58c5c310
Patricia Gorla <gorla.patricia@gmail.com>
2013-10-11 15:57:54 -0400

    Set up stress tests.
    Test out local Usergrid installs.

--- test_stress.py
+++ test_stress.py
@@ -2,4 +2,44 @@
 
 from __future__ import print_function
 
-pass
+import datetime
+import random
+import requests
+import sys
+
+from tokens import ACCESS_TOKEN
+
+ORG = "myorg"
+APP = "myapp"
+
+path = "http://localhost:8080/%s/%s" % (ORG, APP)
+
+headers = {
+    "Authorization": "Bearer {}".format(ACCESS_TOKEN),
+    "Content-Type": "application/json"
+    }
+
+def rand_inserts(inserts=100):
+    success = 0
+    for i in range(inserts):
+        data = {i:random.random()}
+        response = requests.post(path, data=data, headers=headers)
+        if response.status_code == 200:
+            success += 1
+    return success
+
+
+if __name__ == "__main__":
+    try:
+        inserts = int(sys.argv[1])
+    except IndexError:
+        inserts = 1000
+    print("Testing %s inserts." % inserts)
+    tstart = datetime.datetime.now()
+    success = rand_inserts(inserts)
+    tend = datetime.datetime.now()
+
+    ttotal = tend - tstart
+
+    print("Tested %s inserts with %s successes." % (inserts, success))
+    print("Inserted %s inserts in %s seconds." % (success, ttotal.total_seconds()))
---
+++ tokens.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+ACCESS_TOKEN = "YWMtfYTk5DKpEeO5NnnhIA7I4QAAAUHM_m-dpUGg2zTVjwj6JkY5MMfhmaSv3aA"
